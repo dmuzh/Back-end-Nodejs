06c1f47d1a50f26032c5aa2ea53d796f2f50b691
đức mạnh <muzh28@gamil.com>
2023-07-08 10:05:35 +0700

    homework1

--- manualDB/Categories/getById.js
+++ manualDB/Categories/getById.js
@@ -1,7 +1,7 @@
 const { default: mongoose } = require('mongoose');
 const { Category } = require('../../models');
 
-mongoose.connect('mongodb://127.0.0.1:27017/database27-28');
+mongoose.connect('mongodb+srv://muzh28dev:Manh123456@cluster0.heat8ve.mongodb.net/database27-28');
 
 try {
   const taotenphuong = '64675f4da5fb6653ed8bb38b';
